ef912651a1b701975cba5e658c0d4de077ce1ed0
JuanJose <jhonajose@hotmail.es>
2023-01-15 23:41:26 +0100

    Initial Commit

---
+++ EventsIOS/EventsIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsIOSApp.swift
+//  EventsIOS
+//
+//  Created by Juan jose Morales on 15/1/23.
+//
+
+import SwiftUI
+
+@main
+struct EventsIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventsIOSUITests/EventsIOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EventsIOSUITestsLaunchTests.swift
+//  EventsIOSUITests
+//
+//  Created by Juan jose Morales on 15/1/23.
+//
+
+import XCTest
+
+final class EventsIOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
